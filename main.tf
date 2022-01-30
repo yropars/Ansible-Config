0d09ecfc6c51799590e7202435d5d6f319a4ee3a
yropars <93674303+yropars@users.noreply.github.com>
2022-01-30 21:37:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 variable "project_id" {
   type        = string
-  description = "ba193c7e-395f-4e3f-a1f8-048e9e8b7cb4"
+  description = "project_id"
 }
 # Mise en place d'une IP publique
 resource "scaleway_instance_ip" "public_ip" {
