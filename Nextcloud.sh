f690241d21fe98d2993cd276b040a8a05d718cf3
yropars <93674303+yropars@users.noreply.github.com>
2022-01-31 17:53:59 +0100

    Create Nextcloud.sh

--- Nextcloud.sh
+++ Nextcloud.sh
@@ -1,34 +1,32 @@
-  # Téléchargement de Nextcloud
-mkdir /root/test
-wget https://download.nextcloud.com/server/releases/nextcloud-19.0.1.zip
-sudo apt install unzip
-sudo unzip nextcloud-19.0.1.zip -d /var/www/
-sudo chown www-data:www-data /var/www/nextcloud/ -R
+  # Mise à jour des paquets et téléchargement des modules PHP nécessaires
+sudo apt update
+sudo apt install apache2 mariadb-server libapache2-mod-php7.4 --assume-yes
+sudo apt install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl --assume-yes
+sudo apt install php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip --assume-yes
 
-  # Création d'une base de données et d'un utilisateur spécifique pour Nextcloud sur MariaDB
-sudo apt install mariadb-client-core-10.3 --assume-yes
-sudo mysql
-create database nextcloud;
-create user admin@localhost identified by 'epsi2022';
-grant all privileges on nextcloud.* to admin@localhost identified by 'epsi2022';
-flush privileges;
+  # Création de la base de données
+sudo /etc/init.d/mysql start
+sudo mysql -u root -p
 
-   # Création d'un hôte virtuel Apache pour nextcloud
-#sudo nano /etc/apache2/sites-available/nextcloud.conf
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'epsi2022';
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'admin'@'localhost';
+FLUSH PRIVILEGES;
+quit;
 
-  # Activation de l'hôte virtuel et installation des modules PHP
-sudo a2ensite nextcloud.conf
-sudo a2enmod rewrite headers env dir mime setenvif ssl
-sudo systemctl restart apache2
-sudo apt install php-imagick php7.4-common php7.4-mysql php7.4-fpm php7.4-gd php7.4-json php7.4-curl  php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-bcmath php7.4-gmp
-sudo systemctl reload apache2
+  # Suite
+md5sum -c nextcloud-x.y.z.tar.bz2.md5 < nextcloud-x.y.z.tar.bz2
+sha256sum -c nextcloud-x.y.z.tar.bz2.sha256 < nextcloud-x.y.z.tar.bz2
+md5sum  -c nextcloud-x.y.z.zip.md5 < nextcloud-x.y.z.zip
+sha256sum  -c nextcloud-x.y.z.zip.sha256 < nextcloud-x.y.z.zip
 
-  # Activation d'un certificat HTTPS pour Nextcloud
-#sudo apt install certbot python3-certbot-apache
-#sudo certbot --apache --agree-tos --redirect --staple-ocsp --email "votre_email@example.com" -d "nextcloud.example.com"
-#sudo nano /etc/apache2/sites-enabled/nextcloud-le-ssl.conf
-#sudo systemctl reload apache2
+wget https://download.nextcloud.com/server/releases/nextcloud-x.y.z.tar.bz2.asc
+wget https://nextcloud.com/nextcloud.asc
+gpg --import nextcloud.asc
+gpg --verify nextcloud-x.y.z.tar.bz2.asc nextcloud-x.y.z.tar.bz2
 
-  # Finalisation de l'installation
-sudo mkdir /var/www/nextcloud-data
-sudo chown www-data:www-data /var/www/nextcloud-data -R
+tar -xjvf nextcloud-x.y.z.tar.bz2
+unzip nextcloud-x.y.z.zip
+
+cp -r nextcloud /path/to/webserver/document-root
+cp -r nextcloud /var/www
